07bc9cef624b4a6942c749e94ed73cb6477ec310
divonpleasant <78009070+divonpleasant@users.noreply.github.com>
2021-01-27 22:17:19 -0800

    Added files to the correct directory

---
+++ script.js
@@ -0,0 +1,7 @@
+let btnSend = document.querySelector('button');
+let message = document.querySelector('h1');
+
+btnSend.addEventListener('click', () =>{
+    btnSend.innerText = 'Message Sent';
+
+});
